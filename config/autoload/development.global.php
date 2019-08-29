46daf8d3f3fb985712053e1f8f2c34ac0547e26b
Judzhin Miles <info@msbios.com>
2019-08-29 18:16:42 +0300

    Lazy Composer MSBios Commit Script

--- config/autoload/development.global.php
+++ config/autoload/development.global.php
@@ -4,6 +4,30 @@
  * @author Judzhin Miles <info[woof-woof]msbios.com>
  */
 return [
+
+    'navigation' => [
+        'default' => [
+            'home' => [
+                'label' => 'Home',
+                'route' => 'home',
+            ],
+            'page-1' => [
+                'label' => 'Page #1',
+                'uri' => '#page1',
+            ],
+            'page-2' => [
+                'label' => 'Page #2',
+                'uri' => '#page2',
+                'pages' => [
+                    'page-2-1' => [
+                        'label' => 'Page #2.1',
+                        'uri' => '#page2.1',
+                    ],
+                ]
+            ],
+        ],
+    ],
+
     \MSBios\Assetic\Module::class => [
         'maps' => [
             // css
@@ -21,6 +45,12 @@ return [
             // imgs
             'default/img/zf-logo-mark.svg' =>
                 __DIR__ . '/../../themes/default/public/img/zf-logo-mark.svg',
+        ],
+    ],
+
+    'view_manager' => [
+        'strategies' => [
+            'ViewJsonStrategy',
         ],
     ],
 ];
